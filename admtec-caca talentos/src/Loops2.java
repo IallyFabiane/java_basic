4dea47ec38935e791a5b357ccb7bffdd5eb00dc7
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-28 17:06:19 -0300

    estruturas de repetição do while

---
+++ admtec-caca talentos/src/Loops2.java
@@ -0,0 +1,9 @@
+public class Loops2 {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        } while(i < 5);
+    }
+}
