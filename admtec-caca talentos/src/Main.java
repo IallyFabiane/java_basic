648e3604c9674540d324d329e8c2a8b67d2eb38f
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-28 11:00:06 -0300

    caca talentos admtec

---
+++ admtec-caca talentos/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    static void meuMet(String primeiroNome, int idade) {
+        System.out.print(primeiroNome + " ,que tem " + idade + " anos, " + "Eu fui executado!");
+    }
+    public static void main(String[] args) {
+        meuMet("Ially", 28);
+    }
+}
---
+++ admtec-caca talentos/src/Teste.java
@@ -0,0 +1,27 @@
+public class Teste {
+    public static void main(String[] args) {
+        String s = "olá mundo";
+        int i = 1;
+        final double n = 2.72;
+        char c  = 'a';
+        boolean t = true;
+        String saudacao = "Olá";
+        String nome = "Ially";
+        String sobrenome = "Silva";
+        String nomecompleto = nome + " " + sobrenome;
+        String til = "\n\"João\"\n";
+        double a = 7.89;
+        int b = (int) a;
+        System.out.println(s);
+        System.out.println(saudacao.length());
+        System.out.println(saudacao.toUpperCase());
+        System.out.println(saudacao.toLowerCase());
+        System.out.println(saudacao.indexOf("o"));
+        System.out.println(nomecompleto);
+        System.out.print(til);
+        System.out.println(a+"\n"+b);
+    } // comentário unilinha
+}
+/*
+comentário multilinha
+ */
