bc12e4387bf2985babc30f4d515b081c2b13915a
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-28 16:44:21 -0300

    estruturas condicionais if

---
+++ admtec-caca talentos/src/Condicionais.java
@@ -0,0 +1,11 @@
+public class Condicionais {
+    public static void main(String[] args) {
+        if(3<2) {
+            System.out.println("3 é maior que 2");
+        }else if(2 == 2) {
+            System.out.println("2 é igual a 2");
+        }else {
+            System.out.println("3 não é menor que 2");
+        }
+    }
+}
